9e8b2af1be6fb304a6a494dca3101d2853b79871
Kesop1 <kesop1@gmail.com>
2019-05-15 11:55:25 +0200

    Service module removed

--- src/main/java/com/piotrak/Application.java
+++ src/main/java/com/piotrak/Application.java
@@ -1,13 +1,13 @@
-package com.piotrak.Services;
+package com.piotrak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/piotrak/ApplicationTests.java
+++ src/test/java/com/piotrak/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.piotrak.Services;
+package com.piotrak;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
