2ae87db3aade22775e31874b43be6824078d776c
Kesop1 <kesop1@gmail.com>
2019-05-31 16:03:36 +0200

    Security with user login page implemented
    Took 52 minutes

---
+++ src/main/java/com/piotrak/service/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.piotrak.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public ModelAndView getLoginPage() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("loginView");
+        return model;
+    }
+}
