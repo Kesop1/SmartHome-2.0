8ce0e44387e245cf2c04b30c37f28de63a266e17
Kesop1 <krzysztof.piotrak@gmail.com>
2019-07-21 11:23:31 +0200

    Spring security re-enabled

--- src/main/java/com/piotrak/config/WebSecurityConfig.java
+++ src/main/java/com/piotrak/config/WebSecurityConfig.java
@@ -38,8 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
-        /*http
+        http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
@@ -60,7 +59,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessHandler(logoutSuccessHandler())
                 .and()
                 .rememberMe().key("uniqueAndSecret");
-         */
     }
 
     @Bean
