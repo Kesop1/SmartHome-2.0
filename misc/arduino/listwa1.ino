8a773522f1e506474150858d35bb6def406b9487
Kesop1 <krzysztof.piotrak@gmail.com>
2019-11-03 17:12:59 +0100

    Changed the output pins of the relay, added ultrasonic sensor for desk height, added pc button for turning it on, added retained message for connecting the device, updated the fritzing connections file, updated the datasheet

--- misc/arduino/listwa1.ino
+++ misc/arduino/listwa1.ino
@@ -14,6 +14,19 @@
 #include <PubSubClient.h>
 #include <dht.h>
 
+const int SOCKET_1 = 5;//D1
+const int SOCKET_2 = 4;//D2
+const int SOCKET_3 = 0;//D3
+const int SOCKET_4 = 2;//D4
+
+const int ULTRASONIC_TRIG = 14;//D5
+const int ULTRASONIC_ECHO = 12;//D6
+
+const int DESK_UP = 13;//D7
+const int DESK_DOWN = 15;//D8
+
+const int PC_BUTTON = 3;//RX
+
 const char* WIFI_SSID     = "TP-LINK_5DBD15";
 const char* WIFI_PASSWORD = ""; //WIFI password here
 const char* MQTT_SERVER = "192.168.1.101";
@@ -25,13 +38,15 @@ const String MESSAGE_GOODBYE = "disconnected";
 const String SUBTOPIC_IN = "in";
 const String SUBTOPIC_OUT = "out";
 const String SUBTOPIC_STATUS = "status";
+const String TOPIC_PC = "pc";
 const String PUBLISH_TOPIC = DEVICE_NAME + "/" + SUBTOPIC_OUT + "/";
 const String DHT_HUM = "hum";
 const String DHT_TEMP = "temp";
 const String DHT_ = "dht";
 
 const String SUBSCRIBE_TOPICS[] = {
-              DEVICE_NAME + "/" + SUBTOPIC_IN + "/#"
+              DEVICE_NAME + "/" + SUBTOPIC_IN + "/#",
+              TOPIC_PC + "/" + SUBTOPIC_IN
             };
 
 long currentMillis;
@@ -41,6 +56,12 @@ dht DHT;
 long previousReadMillis = 0;
 const long dhtInterval = 3000;
 boolean dhtReadSuccessful = false;
+
+const long DISTANCE_DELTA = 2;
+long deskHeight = 0;
+boolean heightSetSuccessful = true;
+long previousUltrasonicReadMillis = 0;
+const long ultrasonicInterval = 300;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -81,6 +102,9 @@ void loop() {
     if (!dhtReadSuccessful) {
       getDht();
     }
+    if(!heightSetSuccessful){
+      setDeskHeight(deskHeight);
+    }
   }
 }
 
@@ -88,11 +112,16 @@ void loop() {
  * Set up device pins
  */
 void setupPins(){
-  pinMode(4, OUTPUT);//D2
-//  pinMode(5, OUTPUT);//D1
-  pinMode(12, OUTPUT);//D6
-  pinMode(13, OUTPUT);//D7
-  pinMode(14, OUTPUT);//D5
+  pinMode(SOCKET_1, OUTPUT);
+  pinMode(SOCKET_2, OUTPUT);
+  pinMode(SOCKET_3, OUTPUT);
+  pinMode(SOCKET_4, OUTPUT);
+  pinMode(ULTRASONIC_TRIG, OUTPUT);
+  pinMode(DESK_UP, OUTPUT);
+  pinMode(DESK_DOWN, OUTPUT);
+  pinMode(ULTRASONIC_ECHO, INPUT);
+  pinMode(PC_BUTTON, FUNCTION_3);//override RX
+  pinMode(PC_BUTTON, OUTPUT);
 }
 
 /*
@@ -153,7 +182,7 @@ void connectMQTT() {
   if(connectMQTTClient()){
     subscribeMQTT();
     setupPinsFromMQTT();
-    client.publish((DEVICE_NAME + "/" + SUBTOPIC_STATUS).c_str(), MESSAGE_WELCOME.c_str());
+    client.publish((DEVICE_NAME + "/" + SUBTOPIC_STATUS).c_str(), MESSAGE_WELCOME.c_str(), true);
   }
 }
 
@@ -199,10 +228,10 @@ void setupPinsFromMQTT() {
  */
 void activateOfflineMode(){
   Serial.println("Device is offline");
-  digitalWrite(4, HIGH);//D2
-  digitalWrite(12, HIGH);//D6
-  digitalWrite(13, HIGH);//D7
-  digitalWrite(14, HIGH);//D5
+  digitalWrite(SOCKET_1, HIGH);
+  digitalWrite(SOCKET_2, HIGH);
+  digitalWrite(SOCKET_3, HIGH);
+  digitalWrite(SOCKET_4, HIGH);
 }
 
 /*
@@ -219,15 +248,19 @@ void callback(char* top, byte* payload, unsigned int length) {
   if (topic.startsWith(SUBTOPIC_IN)) {
     topic.remove(0, SUBTOPIC_IN.length() + 1);
     String element = topic.substring(0, topic.indexOf("/"));
-    if(element.equals("DHT")){
+    if(element.equalsIgnoreCase("DHT")){
       dhtReadSuccessful = false;
+    } else if(element.equalsIgnoreCase("desk")){
+      int newHeight = msg.toInt();
+      if(newHeight > 0){
+        heightSetSuccessful = false;
+        deskHeight = newHeight;
+      }
     } else {
       int port = element.toInt();
-      if(port > 0){
-        actOnCommand(port, msg);
-        String publishTopic = PUBLISH_TOPIC + port;
-        client.publish(publishTopic.c_str(), msg.c_str(), true);
-      }
+      actOnCommand(port, msg);
+      String publishTopic = PUBLISH_TOPIC + port;
+      client.publish(publishTopic.c_str(), msg.c_str(), true);
     }
   } else if (topic.startsWith(SUBTOPIC_OUT)) {
     topic.remove(0, SUBTOPIC_OUT.length() + 1);
@@ -235,6 +268,12 @@ void callback(char* top, byte* payload, unsigned int length) {
     if(port >0){
       actOnCommand(port, msg);
     }
+  }
+
+  if (((String)top).equalsIgnoreCase(TOPIC_PC + "/" + SUBTOPIC_IN)) {
+    if(msg.equalsIgnoreCase("ON")){
+        turnOnPc();
+      }
   }
 }
 
@@ -245,7 +284,7 @@ void actOnCommand(int port, String msg){
   if(msg.equalsIgnoreCase("ON")){
     digitalWrite(port, HIGH);
   } else if(msg.equalsIgnoreCase("OFF")){
-    digitalWrite(port, LOW);    
+    digitalWrite(port, LOW);
 //  } else{
 //    analogWrite(port, msg.toInt());
   }
@@ -289,4 +328,48 @@ void getDht(){
     client.publish((publishTopic + DHT_HUM).c_str(), hum.c_str(), true);
     client.publish((publishTopic + DHT_TEMP).c_str(), temp.c_str(), true);
   }
+}
+
+/*
+ * Move the desk up or down
+ */
+void setDeskHeight(long height){
+  if (currentMillis - previousUltrasonicReadMillis < ultrasonicInterval) {
+    return;
+  }
+  Serial.println("Setting desk height at " + height);
+  previousUltrasonicReadMillis = currentMillis;
+  digitalWrite(ULTRASONIC_TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC_TRIG, LOW);
+  long ultrasonicDuration = pulseIn(ULTRASONIC_ECHO, HIGH);
+  int distance = ultrasonicDuration*0.034/2;
+  Serial.println("Distance: " + distance);
+
+  if((distance > (height - DISTANCE_DELTA)) && (distance < (height + DISTANCE_DELTA))){
+    Serial.println("Desk set at " + deskHeight);
+    heightSetSuccessful = true;
+    digitalWrite(DESK_UP, LOW);
+    digitalWrite(DESK_DOWN, LOW);
+    return;
+  }
+
+  if(distance > height){
+    Serial.println("Going down");
+    digitalWrite(DESK_UP, HIGH);
+    digitalWrite(DESK_DOWN, LOW);
+  } else {
+    Serial.println("Going up");
+    digitalWrite(DESK_DOWN, HIGH);
+    digitalWrite(DESK_UP, LOW);
+  }
+}
+
+void turnOnPc(){
+  Serial.println("Turning on the pc");
+  digitalWrite(PC_BUTTON, HIGH);
+  delay(100);
+  digitalWrite(PC_BUTTON, LOW);
 }
